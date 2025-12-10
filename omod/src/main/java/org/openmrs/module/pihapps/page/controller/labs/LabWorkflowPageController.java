b9f1c73d0f273c4732f4a76785996e925bd6e512
Michael Seaton <mseaton@pih.org>
2025-12-10 16:24:34 -0500

    Update to reflect new web services and config

--- omod/src/main/java/org/openmrs/module/pihapps/page/controller/labs/LabWorkflowPageController.java
+++ omod/src/main/java/org/openmrs/module/pihapps/page/controller/labs/LabWorkflowPageController.java
@@ -1,15 +1,12 @@
 package org.openmrs.module.pihapps.page.controller.labs;
 
 import org.openmrs.Order;
-import org.openmrs.module.pihapps.orders.LabOrderConfig;
 import org.openmrs.module.pihapps.PihAppsConfig;
+import org.openmrs.module.pihapps.orders.LabOrderConfig;
 import org.openmrs.module.pihapps.orders.OrderStatus;
 import org.openmrs.ui.framework.UiUtils;
 import org.openmrs.ui.framework.annotation.SpringBean;
 import org.openmrs.ui.framework.page.PageModel;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class LabWorkflowPageController {
 
