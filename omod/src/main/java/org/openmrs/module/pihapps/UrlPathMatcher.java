6d74840f00bf9bb632ba68aa598a344a98455201
Michael Seaton <mseaton@pih.org>
2026-02-09 15:57:51 -0500

    UHM-9107 - Whitelist REST for login location

---
+++ omod/src/main/java/org/openmrs/module/pihapps/UrlPathMatcher.java
@@ -0,0 +1,70 @@
+package org.openmrs.module.pihapps;
+
+import org.springframework.util.AntPathMatcher;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+/**
+ * This is a copy of WebUtil from the authentication module.  Copied here due to library conflicts with javax servlet
+ */
+public class UrlPathMatcher {
+
+    private static final AntPathMatcher matcher = new AntPathMatcher();
+
+    static {
+        matcher.setCaseSensitive(false);
+        matcher.setTrimTokens(true);
+    }
+
+    /**
+     * Checks if the request is for a URL that matches at least one of the given patterns.
+     * 
+     * @param request the HttpServletRequest to check
+     * @param patterns the list of URL patterns to check against
+     * @return true if the request is for a URL that matches at least one of the given patterns
+     */
+    public static boolean urlMatchesAnyPattern(HttpServletRequest request, List<String> patterns) {
+        for (String pattern : patterns) {
+            if (matchesPath(request, pattern)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Checks the request servlet path and requestURI against the given pattern.
+     * 
+     * @param request the HttpServletRequest to check
+     * @param pattern the pattern to match
+     * @return true if the request matches the pattern
+     */
+    public static boolean matchesPath(HttpServletRequest request, String pattern) {
+        if (pattern.startsWith("*")) {
+            pattern = "/**/" + pattern;
+        }
+        if (matcher.match(pattern, request.getServletPath())) {
+            return true;
+        }
+        String patternWithContext = contextualizeUrl(request, pattern);
+        return matcher.match(patternWithContext, request.getRequestURI());
+    }
+
+    /**
+     * Appends the OpenMRS context path to the given URL if necessary.
+     * 
+     * @param request the HttpServletRequest containing the context path
+     * @param url the URL to contextualize
+     * @return the URL, prepended with the context path if necessary
+     */
+    public static String contextualizeUrl(HttpServletRequest request, String url) {
+        if (url == null) {
+            url = request.getContextPath();
+        }
+        if (!url.startsWith(request.getContextPath())) {
+            url = request.getContextPath() + (url.startsWith("/") ? "" : "/") + url;
+        }
+        return url;
+    }
+}
