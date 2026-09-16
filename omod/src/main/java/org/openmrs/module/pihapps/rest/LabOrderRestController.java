c9f5d4cdc32c3ee0199a430f4e832def2fd20890
Michael Seaton <mseaton@pih.org>
2026-09-16 14:27:50 -0400

    UHM-9501 - Resolve Obs/Order converters via RestService directly instead of clearing ConversionUtil's cache

--- omod/src/main/java/org/openmrs/module/pihapps/rest/LabOrderRestController.java
+++ omod/src/main/java/org/openmrs/module/pihapps/rest/LabOrderRestController.java
@@ -23,6 +23,7 @@ import org.openmrs.module.webservices.rest.SimpleObject;
 import org.openmrs.module.webservices.rest.web.ConversionUtil;
 import org.openmrs.module.webservices.rest.web.RequestContext;
 import org.openmrs.module.webservices.rest.web.RestUtil;
+import org.openmrs.module.webservices.rest.web.api.RestService;
 import org.openmrs.module.webservices.rest.web.representation.Representation;
 import org.openmrs.module.webservices.rest.web.resource.api.Converter;
 import org.openmrs.module.webservices.rest.web.resource.impl.AlreadyPaged;
@@ -61,12 +62,15 @@ public class LabOrderRestController {
 
     private final ConceptService conceptService;
 
+    private final RestService restService;
+
     @Autowired
-    public LabOrderRestController(PihAppsService pihAppsService, LabOrderConfig labOrderConfig, OrderService orderService, ConceptService conceptService) {
+    public LabOrderRestController(PihAppsService pihAppsService, LabOrderConfig labOrderConfig, OrderService orderService, ConceptService conceptService, RestService restService) {
         this.pihAppsService = pihAppsService;
         this.labOrderConfig = labOrderConfig;
         this.orderService = orderService;
         this.conceptService = conceptService;
+        this.restService = restService;
     }
 
     @RequestMapping(value = "/rest/v1/pihapps/labOrder", method = RequestMethod.GET)
@@ -130,7 +134,7 @@ public class LabOrderRestController {
             Map<Order, Encounter> fulfillerEncounters = pihAppsService.getFulfillerEncountersForOrders(result.getOrders());
             OrderWithFulfillerDetailsResource.primeFulfillerEncounterCache(fulfillerEncounters);
 
-            Converter<Order> orderConverter = ConversionUtil.getConverter(Order.class);
+            Converter<Order> orderConverter = (Converter<Order>) restService.getResourceBySupportedClass(Order.class);
             AlreadyPaged<Order> alreadyPaged = new AlreadyPaged<>(requestContext, result.getOrders(), hasMoreResults, result.getTotalCount());
             return alreadyPaged.toSimpleObject(orderConverter);
         }
